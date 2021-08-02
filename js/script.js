9843f6f42af382c02257d03eb7c94a9e1017c5a4
kimcc <15278020+kimcc@users.noreply.github.com>
2021-08-01 23:54:29 -0500

    add call to get lat and long

--- js/script.js
+++ js/script.js
@@ -1,15 +1,25 @@
-async function getWeather() {
+const getWeather = async function(){
+
+    // Get the latitude and longitude of a location
+    const latLongResponse = await fetch(
+        'https://api.openweathermap.org/data/2.5/weather?q=london&appid=2c46a57714cfaf2673339b7b2f255993'
+    )
+    const latLongData = await latLongResponse.json();
+    const lat = latLongData.coord.lat;
+    const lon = latLongData.coord.lon;
+
+     // Get the location's weather
     const response = await fetch(
-        'https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&units=imperial&appid=2c46a57714cfaf2673339b7b2f255993'
+        `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&appid=2c46a57714cfaf2673339b7b2f255993`
     )
     const data = await response.json();
-    
     const currentTemp = `${data.current.temp}°F`;
 
     console.log(currentTemp);
     getForecast(data);
 }
 
+// Get the 5 day forecast
 const getForecast = function(data) {
 
     data.daily.forEach(function(value, index) {
