582a0824cd0a1298c336ff6f8592ffebbcbba629
kimcc <15278020+kimcc@users.noreply.github.com>
2021-08-03 21:21:13 -0500

    edit recent search and ashow weather info

--- js/script.js
+++ js/script.js
@@ -1,16 +1,31 @@
 const weatherForm = document.querySelector('#weatherForm');
 const weatherInput = document.querySelector('#weatherInput');
+const currentWeather = document.querySelector('#currentWeather');
 const searchList = document.querySelector('#searchList');
+const forecastWeather = document.querySelector('#forecastWeather');
 let searchArray;
 
+// Create the recent search items
 const createRecentSearches = function() {
     const searchData = JSON.parse(localStorage.getItem('searchItem'));
 
-    searchData.forEach(function(item) {
-        const searchText = document.createElement('li');
-        searchText.textContent = item;
-        searchList.appendChild(searchText);
+    // Reverse array to show most recent first
+    searchData.reverse();
+    searchList.innerHTML = '';
+
+    searchData.forEach(function(item, index) {
+        // Show only 5 most recent
+        if (index < 5) {
+            const searchText = document.createElement('li');
+            searchText.textContent = item;
+            searchList.appendChild(searchText);
+        }
     });
+}
+
+// Put recent search text into the input
+const recentSearchHandler = function(event) {
+    weatherInput.value = event.target.textContent;
 }
 
 const formSubmitHandler = function(event) {
@@ -26,6 +41,7 @@ const formSubmitHandler = function(event) {
     weatherInput.value = '';
 }
 
+// Update array by starting from localstorage or create empty one if it doesn't exist
 const saveSearch = function(searchTerm) {
     if (localStorage.getItem('searchItem')) {
         searchArray = JSON.parse(localStorage.getItem('searchItem'))
@@ -49,27 +65,99 @@ const getWeather = async function(location){
     const lat = latLongData.coord.lat;
     const lon = latLongData.coord.lon;
 
-     // Get the location's weather
+    // Get the location's weather
     const response = await fetch(
         `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&appid=2c46a57714cfaf2673339b7b2f255993`
     )
     const data = await response.json();
-    const currentTemp = `${data.current.temp}°F`;
 
-    console.log(currentTemp);
+    // Clear out children from current weather
+    currentWeather.innerHTML = '';
+
+    // Current weather variables
+    const date = document.createElement("p");
+    const unixDate = data.current.dt;
+    const formatDate = {
+        day: 'numeric',
+        month: 'numeric',
+        year: 'numeric'
+    }
+    date.textContent = new Date(unixDate*1000).toLocaleString('en-US', formatDate);
+
+    const temp = document.createElement("p");
+    temp.textContent = `${data.current.temp}°F`;
+
+    const windSpeed = document.createElement("p");
+    windSpeed.textContent = `${data.current.wind_speed} MPH`;
+    
+    const humidity = document.createElement("p");
+    humidity.textContent = `${data.current.humidity}%`;
+
+    const uvIndex = document.createElement("p");
+    uvIndex.textContent = data.current.uvi;
+
+    const icon = document.createElement("img");
+    const iconId = data.current.weather[0].icon;
+    icon.setAttribute('src', `http://openweathermap.org/img/wn/${iconId}@2x.png`);
+    icon.setAttribute('alt', 'Weather icon');
+
+    currentWeather.appendChild(date);
+    currentWeather.appendChild(icon);
+    currentWeather.appendChild(temp);
+    currentWeather.appendChild(humidity);
+    currentWeather.appendChild(windSpeed);
+    currentWeather.appendChild(uvIndex);
+
+    if (data.current.uvi < 3) {
+        uvIndex.classList.add('uv-low');
+    } else if (data.current.uvi >= 3 && data.current.uvi < 8) {
+        uvIndex.classList.add('uv-medium');
+    } else {
+        uvIndex.classList.add('uv-high');
+    }
+    
     getForecast(data);
 }
 
 // Get the 5 day forecast
 const getForecast = function(data) {
 
+    console.log(data.daily);
+
     data.daily.forEach(function(value, index) {
         if (index > 0 &&  index < 6) {
-            const forecastTemp = `${value.temp.day}°F`;
-            console.log(forecastTemp);
+            const forecastDate = document.createElement("p");
+            const unixDate = value.dt;
+            const formatDate = {
+                day: 'numeric',
+                month: 'numeric',
+                year: 'numeric'
+            }
+            forecastDate.textContent = new Date(unixDate*1000).toLocaleString('en-US', formatDate);
+
+            const forecastTemp = document.createElement("p");
+            forecastTemp.textContent = `${value.temp.day}°F`;
+
+            const forecastWindSpeed = document.createElement("p");
+            forecastWindSpeed.textContent = `${value.wind_speed} MPH`;
+
+            const forecastHumidity = document.createElement("p");
+            forecastHumidity.textContent = `${value.humidity}%`;
+
+            const forecastIcon = document.createElement("img");
+            const forecastIconId = value.weather[0].icon;
+            forecastIcon.setAttribute('src', `http://openweathermap.org/img/wn/${forecastIconId}@2x.png`);
+            forecastIcon.setAttribute('alt', 'Weather icon');
+
+            forecastWeather.appendChild(forecastDate);
+            forecastWeather.appendChild(forecastIcon);
+            forecastWeather.appendChild(forecastTemp);
+            forecastWeather.appendChild(forecastWindSpeed);
+            forecastWeather.appendChild(forecastHumidity);
         }
     });
 }
 
-//getWeather();
-weatherForm.addEventListener('submit', formSubmitHandler)
+weatherForm.addEventListener('submit', formSubmitHandler);
+searchList.onclick = recentSearchHandler;
+createRecentSearches();
